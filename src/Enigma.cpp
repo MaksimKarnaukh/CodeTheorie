a7bc36621b47bc7502cfcb4c668fb989568e7af7
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2022-12-09 22:20:27 +0100

    enigma encryption algo implemented, implemented base of decryption algorithm with crib dragging. (CWM session)

--- src/Enigma.cpp
+++ src/Enigma.cpp
@@ -22,5 +22,56 @@ Enigma::Enigma(const std::string &filename) : AlgorithmDecryption(filename) {
 }
 
 std::string Enigma::Solve() {
+//    this->cipherText = "PASOPVOORSALAMANDER";
+    std::string input = this->cipherText;
+
+    // Turing-Bombe
+//    std::array<int, 3> fms{0, 1, 2};
+//    std::string result = sendThrough(input, fms, start_pos, plugBoard);
+//    std::array<int, 26> plugBoard = PermutationStringToArray("PBMEDFLHIZKGCNOAQRSWUVTXYJ");
+
+    std::array<int, 3> start_pos{0,0,0};
+    size_t inputlength =input.length(), criblength = crib.length(), end_index = inputlength-criblength;
+    std::vector<std::array<int,3>> vectorcombs{};
+    std::string sub_string {};
+    GenArrangement(5, 3, 0, 0, 0, vectorcombs);
+    for (const std::array<int, 3>& fms:vectorcombs){ // all possible rotor positions
+        do {
+            for (size_t c = 0; c < end_index; c++) {
+                sub_string = input.substr(c, criblength);
+                // now we have to find a suitable k (see course notes).
+
+            }
+
+            tickRotors(start_pos);
+        } while (start_pos.at(2) != 0 and start_pos.at(1) != 0 and start_pos.at(0) != 0); // loop over all possible rotor configurations
+    }
+
+
+
+
+
+
+
+
+
+    std::cout << "a";
     return std::string();
+}
+
+void Enigma::GenArrangement(int n, int k, int idx, int used, int arran, std::vector<std::array<int,3>> &comb) {
+    if (idx == k) {
+        comb.push_back(std::array<int, 3>({std::to_string(arran)[0], std::to_string(arran)[1], std::to_string(arran)[2]}));
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+        if (0 == (used & (1 << i)))
+            GenArrangement(n, k, idx + 1, used | (1 << i), arran * 10 + (i + 1), comb);
+}
+
+
+
+char Enigma::alphabetIndex(int index) {
+    return (char) (index + ASCII_A);
 }
